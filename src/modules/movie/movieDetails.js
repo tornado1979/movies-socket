8ff72d11d62a799c373870af02ba3a501deb439b
Theodoros Itzaris <theodoreme@gmail.com>
2018-06-04 12:59:10 +0300

    Add 'movieDetails' module

--- src/modules/movie/movieDetails.js
+++ src/modules/movie/movieDetails.js
@@ -1,11 +1,94 @@
 import React, { Component } from 'react'
+// import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import propTypes from 'prop-types'
+import moment from 'moment'
+
+import './movie.scss'
+
+import {
+  getMovie,
+} from '../home/selectors'
+
+import NotFound from '../../modules/notFound/notFound'
 
 class MovieDetails extends Component { // eslint-disable-line
   render() {
+    const {
+      movie,
+    } = this.props
+
+    if (!movie) {
+      return (
+        // <main><div className="wrapper">no movie!</div></main>
+        <main>
+          <NotFound />
+        </main>
+      )
+    }
+
+    const sheets = document.styleSheets
+    const selector = '.movie-wrapper::before'
+    const replacementBG = `url(${movie.full_backdrop_path})`
+
+    // replace background-image
+    /* eslint-disable no-restricted-syntax */
+    for (const sheet of sheets) {
+      if (sheet.href !== 'https://fonts.googleapis.com/css?family=Montserrat') {
+        for (const rule of sheet.cssRules) {
+          if (rule.selectorText === selector) {
+            rule.style.backgroundImage = replacementBG
+          }
+        }
+      }
+    }
+    const movieReleaseDate = moment(movie.release_date)
     return (
-      <div>movie details</div>
+      <main>
+        {movie &&
+          <div className="top-wrapper">
+            <div className="movie-wrapper">
+              <div className="custom-bg">
+                <div className="panel">
+                  <div className="img">
+                    <img alt="movie" src={`${movie.full_poster_path}`} />
+                  </div>
+                  <div className="body">
+                    <div className="wrap-body">
+                      <div>
+                        <p className="title">{movie.title}<span className="date">({movieReleaseDate.format('YYYY')})</span></p>
+                        <p>Original Language ({movie.original_language})</p>
+                      </div>
+                      <div className="overview">
+                        <h3>Overview</h3>
+                        <p>{movie.overview}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>}
+      </main>
     )
   }
 }
 
-export default MovieDetails
+MovieDetails.propTypes = {
+  movie: propTypes.shape().isRequired,
+}
+
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    movie: getMovie(state),
+  }
+}
+
+/*
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getMovie,
+}, dispatch)
+*/
+
+export default connect(mapStateToProps, null)(MovieDetails)
